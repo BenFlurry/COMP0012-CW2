5de113e22a6ca8b683173a8dac95ee2bdb9c9a2f
Sywth <zcabper@ucl.ac.uk>
2025-04-16 01:09:18 +0100

    working m1m2m3m4

--- src/comp0012/target/ConstantVariableFolding.java
+++ src/comp0012/target/ConstantVariableFolding.java
@@ -1,30 +1,32 @@
 package comp0012.target;
+// @formatter:off
 
-public class ConstantVariableFolding
-{
+public class ConstantVariableFolding {
+
     public int methodOne(){
-        int a = 62;
-        int b = (a + 764) * 3;
-        return b + 1234 - a;
+        int a = 62;                 // a <- 62
+        int b = (a + 764) * 3;      // b <- 2478
+        return b + 1234 - a;        // -> 3650
     }
 
     public double methodTwo(){
-        double i = 0.67;
-        int j = 1;
-        return i + j;
+        double i = 0.67;            // i <- 0.67
+        int j = 1;                  // j <- 1
+        return i + j;               // -> 1.67
     }
 
     public boolean methodThree(){
-        int x = 12345;
-        int y = 54321;
-        return x > y;
+        int x = 12345;              // x <- 12345
+        int y = 54321;              // y <- 54321
+        return x > y;               // -> False
     }
 
     public boolean methodFour(){
-        long x = 4835783423L;
-        long y = 400000;
-        long z = x + y;
-        return x > y;
+        long x = 4835783423L;       // x <- 4835783423
+        long y = 400000;            // y <- 400000
+        long z = x + y;             // z <- 4836183423
+        return x > y;               // -> True
     }
 
 }
+// @formatter:on
